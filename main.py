827433013487df90dd6ef863911f5a655f5641c8
AYUSHMAN Mukherjee <127019874+ayush585@users.noreply.github.com>
2025-07-14 22:37:57 +0530

    main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from datetime import datetime
 import os
 import matplotlib.pyplot as plt
 
-# File name
+
 file_name = "expenses.xlsx"
 
-# Step 1: Create Excel if it doesn't exist
+
 if not os.path.exists(file_name):
     df = pd.DataFrame(columns=["Date", "Amount", "Category", "Note"])
     df.to_excel(file_name, index=False)
 else:
     df = pd.read_excel(file_name)
 
-# Step 2: Input from user
+
 try:
     amount = float(input("Enter amount spent (₹): "))
 except ValueError:
@@ -23,7 +23,7 @@ except ValueError:
 category = input("Enter category (Food, Travel, etc.): ").title()
 note = input("Optional note: ")
 
-# Step 3: Add new row
+
 new_entry = {
     "Date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     "Amount": amount,
@@ -34,10 +34,10 @@ df = pd.concat([df, pd.DataFrame([new_entry])], ignore_index=True)
 df.to_excel(file_name, index=False)
 print("✅ Expense saved to Excel!")
 
-# Step 4: Create bar chart
+
 category_data = df.groupby("Category")["Amount"].sum()
 
-# Plotting
+
 plt.figure(figsize=(8, 6))
 category_data.plot(kind="bar", color="skyblue")
 plt.title("Spending by Category")
